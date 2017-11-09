bf3e7bed0c373ece5232635dd172e70e0215b507
Pierre Kisters <pierrekisters@gmail.com>
2017-11-09 11:09:38 +0100

    nano and increased stop timeout from 5 to 9 seconds

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,19 @@ ENV TINI_VERSION 0.14.0
 ENV TINI_URL https://github.com/krallin/tini/releases/download/v$TINI_VERSION/tini
 
 
+ADD ["https://raw.githubusercontent.com/LolHens/docker-tools/master/bin/cleanimage", "/usr/local/bin/"]
+RUN chmod +x "/usr/local/bin/cleanimage"
+
+RUN apt-get update \
+ && apt-get upgrade -y \
+ && apt-get install -y \
+      nano \
+ && cleanimage
+
 RUN curl -Lo "/usr/local/bin/tini" $TINI_URL \
  && chmod +x "/usr/local/bin/tini"
+
+COPY ["bin/run-server.sh", "/run-server.sh"]
 
 
 ENTRYPOINT ["tini", "-g", "--"]
---
+++ bin/run-server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setting default values if variables not present
+: ${TEAMCITY_DIST:=/opt/teamcity}
+: ${TEAMCITY_LOGS:=${TEAMCITY_DIST}/logs}
+: ${TEAMCITY_CONTEXT:=ROOT}
+
+find "${TEAMCITY_LOGS}" -maxdepth 1 -type f -name '*.pid' -exec rm -f '{}' \;
+
+if [[ "$TEAMCITY_CONTEXT" != "ROOT" ]]; then
+    current="$(ls ${TEAMCITY_DIST}/webapps | head -1)"
+    [[ "$current" != "$TEAMCITY_CONTEXT" ]] && mv "${TEAMCITY_DIST}/webapps/$current" "${TEAMCITY_DIST}/webapps/$TEAMCITY_CONTEXT"
+fi
+
+trap "'${TEAMCITY_DIST}/bin/teamcity-server.sh' stop 9; exit 0;" SIGTERM SIGINT SIGHUP
+
+"${TEAMCITY_DIST}/bin/teamcity-server.sh" start
+
+while [ ! -f "${TEAMCITY_DIST}/logs/teamcity-server.log" ];
+do
+   echo -n "."
+   sleep 1
+done
+
+tail -F "${TEAMCITY_DIST}/logs/teamcity-server.log" &
+wait
